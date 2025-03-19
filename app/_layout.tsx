4ef193d18c88cd2da4304c9d010e42daa1b3cfc9
EversonDoNascimento <everson25nascimento@gmail.com>
2025-03-19 16:07:43 -0300

    feat: Add tailwindcss

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
-
+import "../globals.css";
 export default function Layout() {
   return (
     <View>
-      <Text>Layout a</Text>
+      <Text className="text-2xl bg-red-700">Layout a</Text>
     </View>
   );
 }

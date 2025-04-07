d976c191acb412443669dc2ab32382521d060c40
EversonDoNascimento <everson25nascimento@gmail.com>
2025-04-07 17:05:11 -0300

    fix: Added router to list songs

--- components/CardAlbum.tsx
+++ components/CardAlbum.tsx
@@ -1,14 +1,21 @@
-import { Image, Text, View } from "react-native";
+import { Image, Pressable, Text } from "react-native";
 import { AlbumType } from "../types/albumType";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "expo-router";
 
 type Props = {
   data: AlbumType;
 };
 export default function CardAlbum({ data }: Props) {
+  const navigation = useRouter();
   return (
-    <View className="flex flex-col items-center gap-4 justify-center bg-[#12141b] rounded-md p-4">
+    <Pressable
+      onPress={() => {
+        navigation.push(`listSongs/${data.id}`);
+      }}
+      className="flex flex-col items-center gap-4 justify-center bg-[#12141b] rounded-md p-4"
+    >
       {data.cover ? (
         <Image
           source={{ uri: data.cover }}
@@ -22,6 +29,6 @@ export default function CardAlbum({ data }: Props) {
         <FontAwesomeIcon icon={faMusic} size={160} color="white" />
       )}
       <Text className="text-white">{data.title}</Text>
-    </View>
+    </Pressable>
   );
 }
--- components/CardMusic.tsx
+++ components/CardMusic.tsx
@@ -1,29 +1,27 @@
 import { Image, Pressable, Text, View } from "react-native";
-import { musicType } from "../types/musicType";
+import { AudioFile } from "../types/musicType";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
 import { faCompactDisc } from "@fortawesome/free-solid-svg-icons";
 import { faClock } from "@fortawesome/free-solid-svg-icons";
 import formatDuration from "../utils/formatDurationMusic";
-import {
-  AudioMetadataType,
-  AudioWithMetadata,
-} from "../types/audioMetadataType";
+import { AlbumType } from "../types/albumType";
 
 type Props = {
-  data: AudioWithMetadata;
+  data: AudioFile;
+  album: AlbumType | undefined;
 };
-export default function CardMusic({ data }: Props) {
+export default function CardMusic({ data, album }: Props) {
   return (
     <Pressable
       onPress={() => {
-        alert(data.metadata?.common.artist);
+        alert(data.albumId);
       }}
       className=" flex w-full flex-row items-center  gap-5 mb-4 p-4 border-b-slate-600 border-b-[1px]"
     >
-      {data.albumCover ? (
+      {album?.cover ? (
         <Image
-          source={{ uri: data.albumCover }}
+          source={{ uri: album.cover }}
           style={{ width: 60, height: 60, borderRadius: 5, marginRight: 10 }}
         />
       ) : (
@@ -32,17 +30,15 @@ export default function CardMusic({ data }: Props) {
       <View className="gap-2">
         <View className="flex flex-row gap-2  items-center">
           <FontAwesomeIcon icon={faMusic} size={10} color="white" />
-          <Text className="text-white">{data.asset.filename}</Text>
+          <Text className="text-white">{data.filename}</Text>
         </View>
         <View className="flex flex-row gap-2  items-center">
           <FontAwesomeIcon icon={faCompactDisc} size={10} color="white" />
-          <Text className="text-white">{data.metadata?.common.album}</Text>
+          <Text className="text-white">{album?.artist}</Text>
         </View>
         <View className="flex flex-row gap-2  items-center">
           <FontAwesomeIcon icon={faClock} size={10} color="white" />
-          <Text className="text-white">
-            {formatDuration(data.asset.duration)}
-          </Text>
+          <Text className="text-white">{formatDuration(data.duration)}</Text>
         </View>
       </View>
     </Pressable>

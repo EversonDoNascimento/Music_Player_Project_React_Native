148de60f2c8e3790d1b19f09885ba14ec04b2175
EversonDoNascimento <everson25nascimento@gmail.com>
2025-05-10 18:22:15 -0300

    feat: Using songs list saved in device

--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,37 +1,58 @@
-import { FlatList, Text, View } from "react-native";
-import * as MediaLibrary from "expo-media-library";
-import CardMusic from "../../components/CardMusic";
+import {
+  ActivityIndicator,
+  FlatList,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
 import useMusicFiles from "../../utils/listMusicFiles";
-import filteredAudioFiles from "../../utils/filterFormatAudios";
-import { musicType } from "../../types/musicType";
-import { useEffect, useState } from "react";
-import getMusicFiles from "../../utils/listMusicFiles";
-import { useAlbum } from "../../contexts/AlbumContext";
 import CardAlbum from "../../components/CardAlbum";
-
+import { useEffect, useState } from "react";
+import { getSavedSongs } from "../../utils/saveSongs";
+import { AlbumType } from "../../types/albumType";
+import Header from "../../components/Header";
+import { FontAwesome } from "@expo/vector-icons";
 export default function Screen() {
-  const [listAudio, setListAudio] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const { albuns } = useAlbum();
-  const list = useMusicFiles();
-
+  // const { albuns } = useAlbum();
+  const { loading, getMusicFiles } = useMusicFiles();
+  const [savedSongs, setSavedSongs] = useState<AlbumType[]>([]);
+  useEffect(() => {
+    (async () => {
+      const albuns = await getSavedSongs();
+      setSavedSongs(albuns);
+    })();
+  }, []);
   return (
-    <View className="w-full h-full bg-[#1d202c] p-4 relative">
-      {list.loading && <Text>Loading</Text>}
+    <View className="w-full h-full bg-[#1d202c]  relative">
+      <Header>
+        <Pressable
+          className="flex-row items-center gap-2 w-full h-8 justify-center"
+          onPress={() => {
+            getMusicFiles();
+          }}
+        >
+          <FontAwesome name="refresh" size={24} color="white" />
 
-      {/* <FlatList
-        data={list.audioFiles}
-        renderItem={({ item }) => <CardMusic data={item}></CardMusic>}
-        // keyExtractor={(item) => item.asset.
-      ></FlatList> */}
+          <Text className="text-white font-semibold text-xl">Reload</Text>
+        </Pressable>
+      </Header>
+
+      {loading && (
+        <View className="w-full h-full flex-col justify-center items-center  absolute ">
+          <ActivityIndicator size="large" color="white"></ActivityIndicator>
+          <Text className="text-white">Carregando Músicas...</Text>
+        </View>
+      )}
+
       <FlatList
-        data={albuns}
+        data={savedSongs}
         renderItem={({ item }) => <CardAlbum data={item}></CardAlbum>}
         keyExtractor={(item) => item.id}
         numColumns={2}
         columnWrapperStyle={{
           justifyContent: "space-between",
           marginBottom: 10,
+          marginHorizontal: 15,
         }}
       ></FlatList>
     </View>
--- app/listSongs/[id].tsx
+++ app/listSongs/[id].tsx
@@ -1,31 +1,38 @@
-import { useLocalSearchParams } from "expo-router";
-import { FlatList, ScrollView, Text, View } from "react-native";
-import { useEffect, useState } from "react";
-import { useAlbum } from "../../contexts/AlbumContext";
+import { useFocusEffect, useLocalSearchParams } from "expo-router";
+import { FlatList, View } from "react-native";
+import { useCallback, useState } from "react";
 import CardMusic from "../../components/CardMusic";
 import { AlbumType } from "../../types/albumType";
+import { getSavedSongs } from "../../utils/saveSongs";
+import { AudioFile } from "../../types/musicType";
+import Header from "../../components/Header";
 
 const Screen = () => {
   const { id } = useLocalSearchParams();
-  const { albuns } = useAlbum();
   const [album, setAlbum] = useState<AlbumType>();
-  useEffect(() => {
-    () => {
-      const temp = albuns.find((a) => a.id === id);
-      setAlbum(albuns.find((a) => a.id === id));
-      console.log("Tamanho do albun: ", temp?.tracks);
-    };
-  }, [albuns, album]);
+  const [songs, setSongs] = useState<AudioFile[]>([]);
 
+  useFocusEffect(
+    useCallback(() => {
+      (async () => {
+        const songs = await getSavedSongs();
+        const temp = songs.find((a: any) => a.id === id);
+        setAlbum(temp);
+        setSongs(temp.tracks);
+      })();
+
+      return () => {
+        console.log("Saindo da tela");
+      };
+    }, [])
+  );
   return (
     <View className="w-full h-full bg-[#1d202c] p-4 relative">
+      <Header></Header>
       <FlatList
-        data={albuns.find((a) => a.id === id)?.tracks}
+        data={songs}
         renderItem={({ item }) => (
-          <CardMusic
-            data={item}
-            album={albuns.find((a) => a.id === id)}
-          ></CardMusic>
+          <CardMusic data={item} album={album}></CardMusic>
         )}
         keyExtractor={(item) => item.id}
       ></FlatList>
